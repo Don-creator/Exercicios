46c4f46121d4b2c8d777ec9c87813803172b9371
Don-Criador <danitimaooo@gmail.com>
2019-09-23 17:25:43 -0300

    Top

---
+++ Administrador/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Administrador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string admLogin = "admin";
+            string admPass; = "admin";
+
+            Console.Write(" Entre com o Usuario: ");
+            string login = Console.ReadLine();
+
+            Console.Write("Entre com a senha: ");
+           string passwd = Console.ReadLine();
+
+           if ((login == admLogin) && passwd == admPasswd ){
+               Console.WriteLine("Bem Vindo Admin.");
+           } else{
+               Console.WriteLine("Olá usuario.");
+           }
+
+            
+          
+        }
+    }
+}
---
+++ Idade/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Idade
+{
+   class Program
+   {
+       static void Main(string[] args)
+       {
+           int ano = 0;
+           int idade = 0;
+           do{
+           Console.Write("Qual o ano de nascimento: ");
+           ano = int.Parse(Console.ReadLine());
+           idade = 2020-ano;
+               if(ano > 2021)  || (Ano < 0)){
+                Console.WriteLine("Data Inválida !");
+               }
+           } while(ano > 2021);
+           if(idade < 3) {
+               Console.WriteLine("Recem Nascido");
+           } else if((idade >= 3) && (idade <= 11)){
+               Console.WriteLine("Criança");
+           } else if((idade >= 12) && (idade <= 19)){
+               Console.WriteLine("Adolescente");
+           } else if((idade >= 20) && (idade <= 65)){
+               Console.WriteLine("Adulto");
+           } else if(idade > 65){
+               Console.WriteLine("Idoso");
+           } else {
+               Console.Write("Ano inválida!");
+           }
+       }
+   }    
+}
+
+
+
+
